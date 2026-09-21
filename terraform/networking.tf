21a94ed00f84a0aac0db74e5b35229a9dac21f07
Matlaba Machaka <matlabatcmachaka@gmail.com>
2026-09-21 15:28:45 +0200

    Spoke 2

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -51,3 +51,40 @@ resource "azurerm_virtual_network_peering" "spoke_to_hub" {
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
 }
+
+# Workload Spoke 2 Network 
+resource "azurerm_virtual_network" "spoke_two_vnet" {
+  name                = "vnet-${var.prefix}-prod-spoke2"
+  address_space       = ["10.2.0.0/16"] # Shifted address space to prevent overlapping (Hub is 10.0, Spoke1 is 10.1)
+  location            = azurerm_resource_group.gov_core.location
+  resource_group_name = azurerm_resource_group.gov_core.name
+  tags                = azurerm_resource_group.gov_core.tags
+}
+
+# Subnet inside Spoke 2 Network
+resource "azurerm_subnet" "spoke_two_data" {
+  name                 = "sn-spoke2-data"
+  resource_group_name  = azurerm_resource_group.gov_core.name
+  virtual_network_name = azurerm_virtual_network.spoke_two_vnet.name
+  address_prefixes     = ["10.2.1.0/24"]
+}
+
+# Peering: Hub to Spoke 2
+resource "azurerm_virtual_network_peering" "hub_to_spoke_two" {
+  name                      = "peer-hub-to-spoke2"
+  resource_group_name       = azurerm_resource_group.gov_core.name
+  virtual_network_name      = azurerm_virtual_network.hub_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.spoke_two_vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
+
+# Peering: Spoke 2 to Hub
+resource "azurerm_virtual_network_peering" "spoke_two_to_hub" {
+  name                      = "peer-spoke2-to-hub"
+  resource_group_name       = azurerm_resource_group.gov_core.name
+  virtual_network_name      = azurerm_virtual_network.spoke_two_vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.hub_vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
